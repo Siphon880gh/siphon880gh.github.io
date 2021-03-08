5da572b670ae1d3421d28b0878a9cad2d8f53787
Weng Fei Fung <weffung@ucdavis.edu>
2021-03-08 04:00:04 -0800

    React: Navigation link with active style based on active page in the state object

--- assets/js/Components/Navigation.jsx
+++ assets/js/Components/Navigation.jsx
@@ -1,26 +1,35 @@
 class Navigation extends React.Component {
+    constructor(props) {
+      super(props);
+      
+      // default page
+      this.state = {page: "Work"};
+    }
+    isActivePageClass(pageTitle) {
+        return this.state.page===pageTitle?"bg-danger":""
+    }
     render() {
         return (
             <nav data-component="Navigation" className="site-nav">
             <span className="fa fa-grip-lines site-nav-mobile-btn" onclick="this.classNameList.toggle('active');"></span>
             <ul>
                 <li>
-                    <a href="#slide-a">Tech</a>
+                    <a href="#slide-a" className={this.isActivePageClass("Tech")}>Tech</a>
                 </li>
                 <li>
-                    <a href="#about-anchor">WhoAmI</a>
+                    <a href="#about-anchor" className={this.isActivePageClass("WhoAmI")}>WhoAmI</a>
                 </li>
                 <li>
-                    <a href="#work">Work</a>
+                    <a href="#work" className={this.isActivePageClass("Work")}>Work</a>
                 </li>
                 <li>
-                    <a href="#collab">Collab</a>
+                    <a href="#collab" className={this.isActivePageClass("Collab")}>Collab</a>
                 </li>
                 <li>
-                    <a href="#credited">Credited</a>
+                    <a href="#credited" className={this.isActivePageClass("Credited")}>Credited</a>
                 </li>
                 <li>
-                    <a href="#contact">Contact</a>
+                    <a href="#contact" className={this.isActivePageClass("Contact")}>Contact</a>
                 </li>
             </ul>
         </nav>
