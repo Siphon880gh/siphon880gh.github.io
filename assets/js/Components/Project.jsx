04e9355cab417d6696e5f0462caebebb8a1c4875
Weng Fei Fung <weffung@ucdavis.edu>
2021-03-08 08:24:52 -0800

    Github repos link as an overlay

--- assets/js/Components/Project.jsx
+++ assets/js/Components/Project.jsx
@@ -4,11 +4,15 @@ class Project extends React.Component {
     }
     toggleDescription(event) {
         event.preventDefault();
+        event.stopPropagation();
+
         let project = event.target.closest("figure");
         let descs = project.querySelector("figcaption");
-        descs.classList.toggle("display-none");
-        event.preventDefault();
-        event.stopPropagation();
+        let github = project.querySelector("add-github-link");
+        if(descs)
+            descs.classList.toggle("display-none");
+        if(github)
+            github.classList.toggle("display-none");
     }
     render() {
         let repos = "";
@@ -18,16 +22,16 @@ class Project extends React.Component {
             </aside>);
         }
         return (
-            <figure className="project" data-component="Project" onClick={this.openWindow.bind(this)}>
-                <img className="img-contain" src={this.props.screenshot} alt={this.props.title}/>
+            <figure className="project" data-component="Project">
                 {repos}
+                <img className="img-contain" src={this.props.screenshot} alt={this.props.title}/>
                 <figcaption>
                     <h3>{this.props.title}</h3>
                     <p>
                         {this.props.desc}  
                     </p>
                 </figcaption>
-                <div className="overlay overlay-color-1"></div>
+                <div className="overlay overlay-color-1" onClick={this.openWindow.bind(this)}></div>
                 <div className="overlay-toggle-description">
                     <a onClick={this.toggleDescription}><i className='fa fa-eye'></i></a>
                 </div>
