786ad254d60740bf30336b3740fb8f2bfe296dcd
Weng Fei Fung <weffung@ucdavis.edu>
2021-03-08 10:16:33 -0800

    Removed .section-title CSS because we are loading components by clicking Nav links so we can remove the 90 degrees rotated section titles

--- assets/js/Components/Footer.jsx
+++ assets/js/Components/Footer.jsx
@@ -2,10 +2,7 @@ class Footer extends React.Component {
     render() {
         return (
             <footer data-component="Footer" id="contact" className="contact section container-fluid">
-                <h2 className="section-title hidden-xs-up">Contact</h2>
-                <div className="spacer-vertical-small hidden-xs-up"></div>
                 <div className="flex flex-row flex-space-between">
-                    <h3 className="text-light hidden-xs-down">Contact:</h3>
                     <ul className="flex flex-row flex-space-around">
                         <li><a href="tel:3238427514" target="_blank">323-842-7514</a></li>
                         <li><a href="mailto:weffung@ucdavis.edu" target="_blank">weffung@ucdavis.edu</a></li>
