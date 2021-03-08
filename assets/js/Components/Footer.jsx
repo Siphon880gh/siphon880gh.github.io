2d5b7dd87caee0afcb52c3bb3cdf5911ec20800e
Weng Fei Fung <weffung@ucdavis.edu>
2021-03-08 06:16:56 -0800

    React: Credits slide (out of expected order on webpage layout for now)

--- assets/js/Components/Footer.jsx
+++ assets/js/Components/Footer.jsx
@@ -1,12 +1,12 @@
 class Footer extends React.Component {
     render() {
         return (
-            <footer data-component="Footer" id="contact" class="contact section container-fluid">
-                <h2 class="section-title hidden-xs-up">Contact</h2>
-                <div class="spacer-vertical-small hidden-xs-up"></div>
-                <div class="flex flex-row flex-space-between">
-                    <h3 class="text-light hidden-xs-down">Contact:</h3>
-                    <ul class="flex flex-row flex-space-around">
+            <footer data-component="Footer" id="contact" className="contact section container-fluid">
+                <h2 className="section-title hidden-xs-up">Contact</h2>
+                <div className="spacer-vertical-small hidden-xs-up"></div>
+                <div className="flex flex-row flex-space-between">
+                    <h3 className="text-light hidden-xs-down">Contact:</h3>
+                    <ul className="flex flex-row flex-space-around">
                         <li><a href="tel:3238427514" target="_blank">323-842-7514</a></li>
                         <li><a href="mailto:weffung@ucdavis.edu" target="_blank">weffung@ucdavis.edu</a></li>
                         <li><a href="//github.com/Siphon880gh" target="_blank">Github</a></li>
