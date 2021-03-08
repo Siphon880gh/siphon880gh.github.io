7f3819c7ffb452a4d916eb3a05a4dc40fadfb971
Weng Fei Fung <weffung@ucdavis.edu>
2021-03-08 03:04:32 -0800

    React: Header component

--- assets/js/Components/Header.jsx
+++ assets/js/Components/Header.jsx
@@ -1,9 +1,41 @@
 class Header extends React.Component {
     render() {
         return (
-            <div data-component="Header">
-                
-            </div>
+                <header data-component="Header" className="site-header">
+
+                    <div className="page-title-wrapper">
+                        <a href="/">
+                            <h1 className="page-title">Weng Industry</h1>
+                        </a>
+                    </div>
+
+                    <nav class="site-nav">
+                        <span class="fa fa-grip-lines site-nav-mobile-btn" onclick="this.classList.toggle('active');"></span>
+                        <ul>
+                            <li>
+                                <a href="#slide-a">Tech</a>
+                            </li>
+                            <li>
+                                <a href="#about-anchor">WhoAmI</a>
+                            </li>
+                            <li>
+                                <a href="#work">Work</a>
+                            </li>
+                            <li>
+                                <a href="#collab">Collab</a>
+                            </li>
+                            <li>
+                                <a href="#credited">Credited</a>
+                            </li>
+                            <li>
+                                <a href="#contact">Contact</a>
+                            </li>
+                        </ul>
+                    </nav>
+
+                    <span className="clear-fix"></span>
+
+                </header>
         )
     }
 }
