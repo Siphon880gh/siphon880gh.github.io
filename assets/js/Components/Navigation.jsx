1e2b1401abc87ade9246c8e4b771e82f5ae62beb
Weng Fei Fung <weffung@ucdavis.edu>
2021-03-08 04:44:35 -0800

    React: Clicking navigation link updates active page in state object

--- assets/js/Components/Navigation.jsx
+++ assets/js/Components/Navigation.jsx
@@ -5,31 +5,34 @@ class Navigation extends React.Component {
       // default page
       this.state = {page: "Work"};
     }
-    isActivePageClass(pageTitle) {
-        return this.state.page===pageTitle?"bg-danger":""
+    isActivePageClass(linkPage) {
+        return linkPage===this.state.page?"bg-danger":""
     }
-    render() {
+    updateActivePage(event) {
+        this.setState({page:event.target.innerText});
+    }
+    render(props) {
         return (
             <nav data-component="Navigation" className="site-nav">
-            <span className="fa fa-grip-lines site-nav-mobile-btn" onclick="this.classNameList.toggle('active');"></span>
+            <span className="fa fa-grip-lines site-nav-mobile-btn" data-onclick="this.classNameList.toggle('active');"></span>
             <ul>
                 <li>
-                    <a href="#slide-a" className={this.isActivePageClass("Tech")}>Tech</a>
+                    <a href="#slide-a" className={this.isActivePageClass.call(this, "Tech")} onClick={this.updateActivePage.bind(this)}>Tech</a>
                 </li>
                 <li>
-                    <a href="#about-anchor" className={this.isActivePageClass("WhoAmI")}>WhoAmI</a>
+                    <a href="#about-anchor" className={this.isActivePageClass.call(this, "WhoAmI")} onClick={this.updateActivePage.bind(this)}>WhoAmI</a>
                 </li>
                 <li>
-                    <a href="#work" className={this.isActivePageClass("Work")}>Work</a>
+                    <a href="#work" className={this.isActivePageClass.call(this, "Work")} onClick={this.updateActivePage.bind(this)}>Work</a>
                 </li>
                 <li>
-                    <a href="#collab" className={this.isActivePageClass("Collab")}>Collab</a>
+                    <a href="#collab" className={this.isActivePageClass.call(this, "Collab")} onClick={this.updateActivePage.bind(this)}>Collab</a>
                 </li>
                 <li>
-                    <a href="#credited" className={this.isActivePageClass("Credited")}>Credited</a>
+                    <a href="#credited" className={this.isActivePageClass.call(this, "Credited")} onClick={this.updateActivePage.bind(this)}>Credited</a>
                 </li>
                 <li>
-                    <a href="#contact" className={this.isActivePageClass("Contact")}>Contact</a>
+                    <a href="#contact" className={this.isActivePageClass.call(this, "Contact")} onClick={this.updateActivePage.bind(this)}>Contact</a>
                 </li>
             </ul>
         </nav>
