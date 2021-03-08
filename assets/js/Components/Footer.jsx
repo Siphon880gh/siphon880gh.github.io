590583f00d7d9f85d9a0b1d9770d17a148cf7565
Weng Fei Fung <weffung@ucdavis.edu>
2021-03-08 10:26:44 -0800

    Cleaned up CSS to lesser files

--- assets/js/Components/Footer.jsx
+++ assets/js/Components/Footer.jsx
@@ -4,11 +4,12 @@ class Footer extends React.Component {
             <footer data-component="Footer" id="contact" className="contact section container-fluid">
                 <div className="flex flex-row flex-space-between">
                     <ul className="flex flex-row flex-space-around">
-                        <li><a href="tel:3238427514" target="_blank">323-842-7514</a></li>
-                        <li><a href="mailto:weffung@ucdavis.edu" target="_blank">weffung@ucdavis.edu</a></li>
+                        <li><a className="text-bold" href="//linkedin.com/in/weng-fung/" target="_blank">Hire Me!</a></li>
                         <li><a href="//github.com/Siphon880gh" target="_blank">Github</a></li>
-                        <li><a href="//linkedin.com/in/weng-fung/" target="_blank">LinkedIn</a></li>
-                        <li><a href="//www.wengindustry.com" target="_blank">WengIndustry.com</a></li>
+                        <li><a href="mailto:weffung@ucdavis.edu" target="_blank">Email:weffung@ucdavis.edu</a></li>
+                        <li><a href="tel:3238427514" target="_blank">Tel:323-842-7514</a></li>
+                        <li><a href="//www.youtube.com/channel/UCg1O9uttSv3ZBzd1iep25Ig" target="_blank">Youtube</a></li>
+                        <li><a href="//www.wengindustry.com" target="_blank">WengIndustry.com</a></li>                        
                     </ul>
                 </div>
             </footer>
