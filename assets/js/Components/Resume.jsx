0c45917c460da8cb6c72e3b5736be1615514ce57
Weng Fei Fung <weffung@ucdavis.edu>
2021-03-09 07:25:16 -0800

    Resume Doc: Office Apps Live not clearing their cache, so forcing new version of resume by appending v2 to filename

--- assets/js/Components/Resume.jsx
+++ assets/js/Components/Resume.jsx
@@ -2,8 +2,8 @@ class Resume extends React.Component {
     render() {
         return (
                 <div data-component="Resume" className="resume">
-                    <div class="download-resume"><a className="fa-download" href="https://www.wengindustry.com/siphon880gh.github.io/resume/Fung, Weng Fei - Resume - Web Developer.docx">&nbsp;Download resume</a></div>
-                    <iframe src="https://view.officeapps.live.com/op/embed.aspx?src=https://www.wengindustry.com/siphon880gh.github.io/resume/Fung, Weng Fei - Resume - Web Developer.docx"></iframe>
+                    <div class="download-resume"><a className="fa-download" href="https://www.wengindustry.com/siphon880gh.github.io/resume/Fung, Weng Fei - Resume - Web Developer.v2.docx">&nbsp;Download resume</a></div>
+                    <iframe src="https://view.officeapps.live.com/op/embed.aspx?src=https://www.wengindustry.com/siphon880gh.github.io/resume/Fung, Weng Fei - Resume - Web Developer.v2.docx"></iframe>
                 </div>
         )
     }
