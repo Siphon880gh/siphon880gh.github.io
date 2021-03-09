f40efee55f3f03606db43b7b0811ae9a72a6dfcc
Weng Fei Fung <weffung@ucdavis.edu>
2021-03-08 22:52:20 -0800

    Resume component

--- assets/js/Components/Resume.jsx
+++ assets/js/Components/Resume.jsx
@@ -2,7 +2,8 @@ class Resume extends React.Component {
     render() {
         return (
                 <div data-component="Resume" className="resume">
-                    <iframe href="resume/Fung, Weng Fei - Resume - Web Developer.docx"></iframe>
+                    <div class="download-resume"><a className="fa-download" href="https://www.wengindustry.com/siphon880gh.github.io/resume/Fung, Weng Fei - Resume - Web Developer.docx">Download resume</a></div>
+                    <iframe src="https://view.officeapps.live.com/op/embed.aspx?src=https://www.wengindustry.com/siphon880gh.github.io/resume/Fung, Weng Fei - Resume - Web Developer.docx"></iframe>
                 </div>
         )
     }
