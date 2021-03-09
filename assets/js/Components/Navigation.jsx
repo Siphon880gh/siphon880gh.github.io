3bfab30252d4cb3093a48051b310de127f7652bb
Weng Fei Fung <weffung@ucdavis.edu>
2021-03-08 22:01:15 -0800

    Mobile: Mobile menu collapses back when you select a menu item

--- assets/js/Components/Navigation.jsx
+++ assets/js/Components/Navigation.jsx
@@ -17,25 +17,30 @@ class Navigation extends React.Component {
         icon.classList.toggle("active");
     }
 
+    collapseMobileMenu() {
+        let hamburgerIcon = document.querySelector(".site-nav-mobile-btn");
+        hamburgerIcon.classList.remove("active");
+    }
+
     render(props) {
         return (
             <nav data-component="Navigation" className="site-nav">
             <span className="fa fa-grip-lines site-nav-mobile-btn" onClick={this.onClickMobileHamburgerIcon}></span>
             <ul>
                 <li>
-                    <NavLink to="./tech" activeClassName="text-bold-colored">Tech</NavLink>
+                    <NavLink to="./tech" activeClassName="text-bold-colored" onClick={this.collapseMobileMenu}>Tech</NavLink>
                 </li>
                 <li>
-                    <NavLink to="./whoami" activeClassName="text-bold-colored">WhoAmI</NavLink>
+                    <NavLink to="./whoami" activeClassName="text-bold-colored" onClick={this.collapseMobileMenu}>About Me</NavLink>
                 </li>
                 <li>
-                    <NavLink to="./work" activeClassName="text-bold-colored">Work</NavLink>
+                    <NavLink to="./work" activeClassName="text-bold-colored" onClick={this.collapseMobileMenu}>Work</NavLink>
                 </li>
                 <li>
-                    <NavLink to="./collab" activeClassName="text-bold-colored">Collab</NavLink>
+                    <NavLink to="./collab" activeClassName="text-bold-colored" onClick={this.collapseMobileMenu}>Collab</NavLink>
                 </li>
                 <li>
-                    <NavLink to="./credited" activeClassName="text-bold-colored">Credited</NavLink>
+                    <NavLink to="./credited" activeClassName="text-bold-colored" onClick={this.collapseMobileMenu}>Credited</NavLink>
                 </li>
             </ul>
         </nav>
