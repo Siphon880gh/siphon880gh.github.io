234e8bf2fb7cfe891977b0514f4e8b216e4908ac
Weng Fei Fung <weffung@ucdavis.edu>
2021-03-15 05:19:50 -0700

    Updated Resume

--- src/assets/js/Components/Resume.js
+++ src/assets/js/Components/Resume.js
@@ -6,8 +6,8 @@ export default class Resume extends React.Component {
     render() {
         return (
                 <div data-component="Resume" className="resume">
-                    <iframe src="https://view.officeapps.live.com/op/embed.aspx?src=https://www.wengindustry.com/siphon880gh.github.io/docs/resume/Fung, Weng Fei - Resume - Web Developer.v3.docx"></iframe>
-                    <div class="download-resume"><a className="fa-download" href="https://www.wengindustry.com/siphon880gh.github.io/docs/resume/Fung, Weng Fei - Resume - Web Developer.v3.docx">&nbsp;Download resume</a></div>
+                    <iframe src="https://view.officeapps.live.com/op/embed.aspx?src=https://www.wengindustry.com/siphon880gh.github.io/docs/resume/Fung, Weng Fei - Resume - Learning Assistant.docx"></iframe>
+                    <div class="download-resume"><a className="fa-download" href="https://www.wengindustry.com/siphon880gh.github.io/docs/resume/Fung, Weng Fei - Resume - Learning Assistant.docx">&nbsp;Download resume</a></div>
                 </div>
         )
     }
