1cefee45dc396f7d50cf2c3eb0d533c549597b43
Weng Fei Fung <weffung@ucdavis.edu>
2021-04-01 17:49:42 -0700

    Updated Resumed

--- src/assets/js/Components/WhoAmI.js
+++ src/assets/js/Components/WhoAmI.js
@@ -17,22 +17,23 @@ export default class WhoAmI extends React.Component {
 
                                             <h2 id="about-anchor" className="f1a">About  <address className="author">Weng Fei Fung</address></h2>
                                             <div className="f1b">
-                                                <p>Passionate about all things coding 💻 especially web development 🌎, Weng has made this a hobby and side job for countless years. Have worked with startups including Mixotype, companies like ExRx, and small gigs by word of mouth. He's currently in the <span className="ucla">UCLA</span> Coding Bootcamp.</p>
-                                                <p>He is looking forward to making this a full time job and is working on credentials to do so. However, he is available for hire right away. Look below for his previous work and testimonials.</p>
+                                                <p>Passionate about all things coding 💻 especially web development 🌎, Weng has made this a hobby and side job for countless years. Have worked with startups including Mixotype, companies like ExRx, and small gigs by word of mouth. He graduated from <span className="ucla">UCLA</span>-Trilogy Coding Bootcamp on 4/8/2021.</p>
+                                                <p>He is looking forward to working with your company as a full stack web developer or related web developer role. Look at my LinkedIn for client testimonials.</p>
                                                 <p>You can see his passion on other links:</p>
 
                                                 <aside className="badges">
 
+                                                    <a target="_blank" href="https://www.linkedin.com/in/weng-fung/" rel="nofollow"><img src="https://camo.githubusercontent.com/0f56393c2fe76a2cd803ead7e5508f916eb5f1e62358226112e98f7e933301d7/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f4c696e6b6564496e2d626c75653f7374796c653d666c6174266c6f676f3d6c696e6b6564696e266c6162656c436f6c6f723d626c7565"
+                                                            className="github-style badge-style" alt="Linked-In" data-canonical-src="https://img.shields.io/badge/LinkedIn-blue?style=flat&amp;logo=linkedin&amp;labelColor=blue"/></a>
+
                                                     <a target="_blank" href="https://github.com/Siphon880gh" rel="nofollow"><img src="https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white" alt="Github" data-canonical-src="https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white"/></a>
+
 
                                                     <a target="_blank" href="https://www.youtube.com/user/Siphon880yt/" rel="nofollow">
                                                         <img src="https://camo.githubusercontent.com/0bf5ba8ac9f286f95b2a2e86aee46371e0ac03d38b64ee2b78b9b1490df38458/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f596f75747562652d7265643f7374796c653d666c6174266c6f676f3d796f7574756265266c6162656c436f6c6f723d726564"
                                                             className="github-style badge-style" alt="Youtube" data-canonical-src="https://img.shields.io/badge/Youtube-red?style=flat&amp;logo=youtube&amp;labelColor=red"/>
                                                             
                                                             </a>
-
-                                                    <a target="_blank" href="https://www.linkedin.com/in/weng-fung/" rel="nofollow"><img src="https://camo.githubusercontent.com/0f56393c2fe76a2cd803ead7e5508f916eb5f1e62358226112e98f7e933301d7/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f4c696e6b6564496e2d626c75653f7374796c653d666c6174266c6f676f3d6c696e6b6564696e266c6162656c436f6c6f723d626c7565"
-                                                            className="github-style badge-style" alt="Linked-In" data-canonical-src="https://img.shields.io/badge/LinkedIn-blue?style=flat&amp;logo=linkedin&amp;labelColor=blue"/></a>
                                                 </aside>
 
                                             </div>
