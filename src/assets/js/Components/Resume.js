e5ec93f690ffc2e7862f32a8ca46ee712d0893a2
Weng Fei Fung <weffung@ucdavis.edu>
2021-04-14 17:11:59 -0700

    Updated resume

--- src/assets/js/Components/Resume.js
+++ src/assets/js/Components/Resume.js
@@ -2,12 +2,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const filename = "Fung, Weng Fei - Resume - Central Grader or Tutor - Transitional v1.2.docx";
+const baseLink = "https://www.wengindustry.com/siphon880gh.github.io/docs/resume/";
+const docLink = baseLink + filename;
+
 export default class Resume extends React.Component {
     render() {
         return (
                 <div data-component="Resume" className="resume">
-                    <iframe src="https://view.officeapps.live.com/op/embed.aspx?src=https://www.wengindustry.com/siphon880gh.github.io/docs/resume/Fung, Weng Fei - Resume - Central Grader or Tutor.v2.docx"></iframe>
-                    <div class="download-resume"><a className="fa-download" href="https://www.wengindustry.com/siphon880gh.github.io/docs/resume/Fung, Weng Fei - Resume - Central Grader or Tutor.v2.docx">&nbsp;Download resume</a></div>
+                    <iframe src={`https://view.officeapps.live.com/op/embed.aspx?src=${docLink}`}></iframe>
+                    <div class="download-resume"><a className="fa-download" href={docLink}>&nbsp;Download resume</a></div>
                 </div>
         )
     }
