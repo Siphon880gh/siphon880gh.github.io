6521eadf7aa91e74711be52d1896c4a4ee5a4eed
Weng Fei Fung <weffung@ucdavis.edu>
2021-04-17 20:46:07 -0700

    Updated resume

--- src/assets/js/Components/Resume.js
+++ src/assets/js/Components/Resume.js
@@ -3,7 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const baseLink = "https://www.wengindustry.com/me/docs/resume/";
-const filename = "Fung, Weng Fei - Resume - Central Grader or Tutor - Transitional v1.3.docx";
+const filename = "Fung, Weng Fei - Resume - Central Grader or Tutor - Transitional v1.4.docx";
 const docLink = encodeURIComponent(baseLink + filename);
 
 export default class Resume extends React.Component {
