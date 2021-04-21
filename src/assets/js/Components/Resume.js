260d239af22a56b4fdc59dc40e57c8de1068f85a
Weng Fei Fung <weffung@ucdavis.edu>
2021-04-20 17:38:57 -0700

    Fixed Bug: Resume download not working

--- src/assets/js/Components/Resume.js
+++ src/assets/js/Components/Resume.js
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom";
 
 const baseLink = "https://www.wengindustry.com/me/docs/resume/";
 const filename = "Fung, Weng Fei - Resume - Central Grader or Tutor - Transitional v1.4.docx";
-const docLink = encodeURIComponent(baseLink + filename);
+const docLink = baseLink + filename;
 
 export default class Resume extends React.Component {
     render() {
