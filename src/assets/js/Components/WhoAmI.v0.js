4709c4b7123b264ad1f3744e3fa453da59cfea29
Weng Fei Fung <weffung@ucdavis.edu>
2022-10-28 05:18:20 -0700

    Rearranged Who Am I

---
+++ src/assets/js/Components/WhoAmI.v0.js
@@ -0,0 +1,53 @@
+
+import React from "react";
+import ReactDOM from "react-dom";
+import {NavLink} from "react-router-dom";
+
+export default class WhoAmI extends React.Component {
+    render() {
+        return (
+                <div data-component="WhoAmI" id="slide-b" className="slide slide-b">
+
+                    <div className="wrapper max-width">
+
+                    <div className="a"></div>
+                            <div className="b">
+                                <span className="bb1"></span>
+                                <span className="bb2">
+                                        <div className="flexie mt-2">
+
+                                            {/* <h2 id="about-anchor" className="f1a"><address className="author">Weng Fei Fung</address></h2> */}
+                                            <div className="f1b">
+                                                {/* <p>Passionate about all things coding 💻 especially web development 🌎, Weng has made this a hobby and side job for five years. Have worked with startups including Mixotype, companies like ExRx, and small gigs by word of mouth (with jQuery/Php/MySQL). Certified in MERN (MongoDB, Mongoose, Express, React, Node) and GraphQL with top marks at <span className="ucla">UCLA</span>-Trilogy Coding Bootcamp.</p>
+                                                <p>He is looking forward to working with your company as a web developer. See <NavLink to="./testimonials">Testimonials</NavLink>.</p> */}
+                                                
+                                                <h2 id="about-anchor" className="f1a">Full Stack Developer<br/><address className="author">Weng Fei Fung</address></h2>
+                                                <p>Full stack developer with a background in nursing. 5 years of freelancing jQuery/PHP/MySQL. Certified with top marks at UCLA for MERN stack (MongoDB/Mongoose, Express, React, Node) and GraphQL. Have worked with startups including Mixotype, and companies like ExRx and People's Care. Known as an innovative problem solver, developer friends go to me when they are stuck or have startup ideas. My strong points are jQuery, React, PHP, Express JS, and Node. My strength is in listening to business requirements and translating them into real products. In the future I'd like to take a more entrepreneurship and management role combining healthcare and programming in the hospital setting, as I saw a need for charting software made by healthcare professionals.</p>
+
+                                                <aside className="badges">
+
+                                                    <a target="_blank" href="https://www.linkedin.com/in/weng-fung/" rel="nofollow"><img src="https://camo.githubusercontent.com/0f56393c2fe76a2cd803ead7e5508f916eb5f1e62358226112e98f7e933301d7/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f4c696e6b6564496e2d626c75653f7374796c653d666c6174266c6f676f3d6c696e6b6564696e266c6162656c436f6c6f723d626c7565"
+                                                            className="github-style badge-style" alt="Linked-In" data-canonical-src="https://img.shields.io/badge/LinkedIn-blue?style=flat&amp;logo=linkedin&amp;labelColor=blue"/></a>
+
+                                                    <a target="_blank" href="https://github.com/Siphon880gh" rel="nofollow"><img src="https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white" alt="Github" data-canonical-src="https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white"/></a>
+
+
+                                                    <a target="_blank" href="https://www.youtube.com/user/Siphon880yt/" rel="nofollow">
+                                                        <img src="https://camo.githubusercontent.com/0bf5ba8ac9f286f95b2a2e86aee46371e0ac03d38b64ee2b78b9b1490df38458/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f596f75747562652d7265643f7374796c653d666c6174266c6f676f3d796f7574756265266c6162656c436f6c6f723d726564"
+                                                            className="github-style badge-style" alt="Youtube" data-canonical-src="https://img.shields.io/badge/Youtube-red?style=flat&amp;logo=youtube&amp;labelColor=red"/>
+                                                    </a>
+                                                    
+                                                </aside>
+
+                                                <aside class="info-spacer" style={{width:"1px", height:"10px"}}></aside>
+
+                                            </div>
+                                        </div>
+                                    
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+        )
+    }
+}
