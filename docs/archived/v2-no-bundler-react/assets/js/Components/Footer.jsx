8eedd2a130e4ba04e9ccae82e3e13494111bd269
Siphon880gh <weng.f.fung@gmail.com>
2024-07-31 23:11:40 -0700

    Updated social links - more

--- docs/archived/v2-no-bundler-react/assets/js/Components/Footer.jsx
+++ docs/archived/v2-no-bundler-react/assets/js/Components/Footer.jsx
@@ -8,7 +8,7 @@ class Footer extends React.Component {
                         <li><a href="//github.com/Siphon880gh" target="_blank">Github</a></li>
                         <li><a href="mailto:weffung@ucdavis.edu" target="_blank">Email:weffung@ucdavis.edu</a></li>
                         <li><a href="tel:3238427514" target="_blank">Tel:323-842-7514</a></li>
-                        <li><a href="//www.youtube.com/channel/UCg1O9uttSv3ZBzd1iep25Ig" target="_blank">Youtube</a></li>
+                        <li><a href="//www.youtube.com/@WayneTeachesCode/" target="_blank">Youtube</a></li>
                         <li><a href="//www.wengindustry.com" target="_blank">WengIndustry.com</a></li>                        
                     </ul>
                 </div>
